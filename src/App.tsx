d14e0966485b69f9fd89be7e8e4a644eac5f0903
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-08-26 08:45:18 +0900

    カスタムフックを使わないデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import axios from "axios";
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/UserProfile";
+import { useState } from "react";
 
 export default function App() {
   const user = {
@@ -8,9 +12,29 @@ export default function App() {
     email: "abc@aa.com",
     address: "東京"
   };
+
+  const [userProfile, setUserProfile] = useState<Array<UserProfile>>([]);
+
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.street}${user.address.suite}${user.address.city}`
+        }));
+        setUserProfile(data);
+      });
+  };
+
   return (
     <div className="App">
-      <UserCard user={user} />
+      <button onClick={onClickFetchUser}>表示</button>
+      {userProfile.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -9,7 +9,9 @@ export const UserCard: VFC<Props> = (props) => {
   const { user } = props;
   const style = {
     border: "solid 1px #ccc",
-    borderRadius: "8px"
+    borderRadius: "8px",
+    padding: "0 16px",
+    margin: "8px"
   };
   return (
     <div style={style}>
